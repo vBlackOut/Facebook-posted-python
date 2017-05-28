c21e9c54ce7de677db9d1a0d7034819452a9f82f
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2017-05-28 19:03:41 +0200

    add the get friend profil

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from pyvirtualdisplay import Display
-
+ 
 # Dependency for wait element
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
